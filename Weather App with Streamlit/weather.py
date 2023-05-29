cac7caa1e302b935dbfedc5e002e1b3bf049b1ec
Saadat Khalid Awan <126876532+Saadat-Khalid@users.noreply.github.com>
2023-05-29 15:07:25 +0500

    Update weather.py

--- Weather App with Streamlit/weather.py
+++ Weather App with Streamlit/weather.py
@@ -1,3 +1,11 @@
+import subprocess
+
+def install_packages():
+    subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
+
+if __name__ == '__main__':
+    install_packages()
+
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
